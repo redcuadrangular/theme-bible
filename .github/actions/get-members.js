2ae21c895e0c03dc2602d9ed8e472a847c97d6e8
Neftali Yagua <neftaliyagua@fedoraproject.org>
2024-01-31 06:32:07 -0500

    Github action octokit

--- .github/actions/get-members.js
+++ .github/actions/get-members.js
@@ -3,11 +3,12 @@ const { Octokit } = require("@octokit/action");
 const octokit = new Octokit();
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
-const { data } = await octokit.request('GET /orgs/{org}/members', {
+octokit.request('GET /orgs/{org}/members', {
   org: owner,
   headers: {
     'X-GitHub-Api-Version': '2022-11-28'
   }
+}).then(( data )=>{
+  console.dir(data);
 });
 
-console.dir(data);
